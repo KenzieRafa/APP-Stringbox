5abf8f3adbcc90b09c460b57e35d812dac7bf527
rasyidrizky <rasyid.rsn@gmail.com>
2025-11-22 18:46:16 +0700

    finish

---
+++ app/src/main/java/com/example/stringbox_splashscreen/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.stringbox_splashscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.stringbox_splashscreen.ui.theme.StringboxsplashscreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContent {
+            StringboxsplashscreenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StringboxsplashscreenTheme {
+        Greeting("Android")
+    }
+}
